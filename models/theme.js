28e8b6ad0254061b6d5a708498e0cfc990a16443
Nilesh Gautam <gautamnilesh084@gmail.com>
2023-03-07 17:37:18 +0530

    optimsed some parts

--- models/theme.js
+++ models/theme.js
@@ -1,87 +1,85 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const ThemeSchema = new Schema({
-    user : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
-    },
-
-    email : {
-        type: String,
-        required : true
-    },
-    college : {
-        type: String,
-        required : true
-    },
-    theme : {
-        type :String,
-        default:"Not participating"
-    },
-    group_dance :{
-        type: [String], 
-        default : "Not participating"
-    },
-    fashion_show :{
-        type: [String], 
-        default : "Not participating"
-    },
-    mr_ms :{
-        type: [String], 
-        default : "Not participating"
-    },
-    tug_of_war :{
-        type: [String], 
-        default : "Not participating"
-    },
-    surprice_event :{
-        type: String, 
-        default : "Not participating"
-    },
-    war_of_word :{
-        type: String, 
-        default : "Not participating"
-    },
-    third_degree :{
-        type: String, 
-        default : "Not participating"
-    },
-    meme_making :{
-        type: String, 
-        default : "Not participating"
-    },
-    code_hunt :{
-        type: [String], 
-        default : "Not participating"
-    },
-    blind_coding :{
-        type: [String], 
-        default : "Not participating"
-    },
-    photography :{
-        type: String, 
-        default : "Not participating"
-    },  
-    cinematic_creation :{
-        type: [String], 
-        default : "Not participating"
-    },
-    cs_go :{
-        type: [String], 
-        default : "Not participating"
-    },
-    nfs :{
-        type: String, 
-        default : "Not participating"
-    },
-    fifa :{
-        type: String, 
-        default : "Not participating"
-    }
-  
-})
-
+ 
+  email: {
+    type: String,
+    required: true,
+  },
+  college: {
+    type: String,
+    required: true,
+  },
+  theme: {
+    type: String,
+    default: "Not participating",
+  },
+  group_dance: {
+    type: [String],
+    default: "Not participating",
+  },
+  fashion_show: {
+    type: [String],
+    default: "Not participating",
+  },
+  mr_ms: {
+    type: [String],
+    default: "Not participating",
+  },
+  tug_of_war: {
+    type: [String],
+    default: "Not participating",
+  },
+  surprice_event: {
+    type: String,
+    default: "Not participating",
+  },
+  war_of_word: {
+    type: String,
+    default: "Not participating",
+  },
+  third_degree: {
+    type: String,
+    default: "Not participating",
+  },
+  meme_making: {
+    type: String,
+    default: "Not participating",
+  },
+  code_hunt: {
+    type: [String],
+    default: "Not participating",
+  },
+  blind_coding: {
+    type: [String],
+    default: "Not participating",
+  },
+  photography: {
+    type: String,
+    default: "Not participating",
+  },
+  cinematic_creation: {
+    type: [String],
+    default: "Not participating",
+  },
+  cs_go: {
+    type: [String],
+    default: "Not participating",
+  },
+  nfs: {
+    type: String,
+    default: "Not participating",
+  },
+  fifa: {
+    type: String,
+    default: "Not participating",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model("theme", ThemeSchema);
